4c88f3f22dc79af458c57ef92f139c863b4323bb
@willhead <60652390+will-head@users.noreply.github.com>
2020-08-17 16:36:22 +0100

    Adds Travis deploy details

---
+++ deploy/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ "$1" = "production" ]] ; then
+  DEPLOY_DIR="ppoc.dev"
+else
+  DEPLOY_DIR="staging.ppoc.dev"
+fi
+
+echo "Deploying files to server"
+echo "[${DEPLOY_DOMAIN}]:${DEPLOY_PORT} ecdsa-sha2-nistp256 ${DEPLOY_KEY}" >> ${HOME}/.ssh/known_hosts
+# ssh ${DEPLOY_USER}@${DEPLOY_DOMAIN} -p ${DEPLOY_PORT}
+rsync -avP -e "ssh -p ${DEPLOY_PORT}" build/ ${DEPLOY_USER}@${DEPLOY_DOMAIN}:/home/${DEPLOY_USER}/domains/${DEPLOY_DIR}/
+echo "Deployment complete"
